a5cf64cfa2db8823fa45f98d7e49c5050827b54e
Josvid Calderon <Josvid.calderon@sinco.com.co>
2022-08-29 12:22:44 -0500

    init project dropdown fruits

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Dropdown/>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Dropdown from './components/dropdownFruits.vue'
+import 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+// import 'material-design-icons/iconfont/material-icons.css'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Dropdown
   }
 }
 </script>
 
 <style>
+body{
+  background: #A0AEC0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* background: #A0AEC0; */
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* color: #2c3e50;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/dropdownFruits.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row">
+    <div class="col s6 m3">
+      <div class="card">
+        
+        <div class="card-content">
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DropdownFruits'
+  
+}
+</script>
+
+<style>
+
+</style>
